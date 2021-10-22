18f94253a2f72ee4259f03df4dbe030d35971f4c
TeachY0u <marianoteach@rocketmail.com>
2021-10-21 20:54:43 -0700

    ading to db and service worker

---
+++ Develop/public/db.js
@@ -0,0 +1,7 @@
+let db;
+const request = indexDB.open("budget", 1);
+
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
