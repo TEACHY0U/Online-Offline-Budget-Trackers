d3f4d971bd8dc2b26752283f52a95ad5df7007cb
TeachY0u <marianoteach@rocketmail.com>
2021-10-21 21:03:25 -0700

    adding function

--- Develop/public/db.js
+++ Develop/public/db.js
@@ -1,7 +1,11 @@
 let db;
 const request = indexDB.open("budget", 1);
 
-request.onupgradeneeded = function (event) {
-  const db = event.target.result;
+request.onUpgradeNeeded = function (e) {
+  const db = e.target.result;
   db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onSuccess = function (e) {
+  db = e.target.result;
 };
